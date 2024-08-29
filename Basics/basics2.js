d0645d1c653589c54afb925943337419a039b71b
DigvijaySinghRajput <rajputdd15@gmail.com>
2024-08-29 13:43:06 +0000

    basics2

---
+++ Basics/basics2.js
@@ -0,0 +1,14 @@
+let no = 123;
+let surname = "Rajput";
+let = null;
+let state;
+let flag = true;
+
+
+console.log(typeof no); //number range 2^53
+console.log(typeof surname);
+console.log(typeof state);
+console.log(typeof flag);
+console.log(typeof null);
+
+
