c66cd7670465dfd2666e0c3a4033a656c77eadd0
DigvijaySinghRajput <rajputdd15@gmail.com>
2024-09-05 16:14:44 +0000

    math and num

---
+++ Basics/math and num.js
@@ -0,0 +1,37 @@
+const score = 400;
+console.log(score)
+
+const balance = new Number(10.253)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+const otherName = 123.595
+console.log(otherName.toPrecision(4))
+const hundred = 100000000
+console.log(hundred.toLocaleString('en-IN'))
+
+//*******************************MATHS**********************************
+console.log(Math)
+console.log(Math.abs(-45))
+console.log(Math.round(4.55))
+console.log(Math.ceil(4.55))
+console.log(Math.floor(4.55))
+
+console.log(Math.min(4,8,7,9))
+console.log(Math.max(4,8,7,9))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*( max - min + 1)) + min)
+
+
+
+
